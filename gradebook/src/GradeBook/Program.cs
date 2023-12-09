a9bb051e5725a28c9a4dc4d0f5a245e59c276f1d
Chandra <30cpss@gmail.com>
2023-12-09 19:18:11 +0530

    add old pattern

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Chandra is here");
+﻿using System;
+
+namespace GradeBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length>0){
+                Console.WriteLine("hello chandra");
+            }
+            Console.WriteLine("Hello World!");
+
+            var numbers = new[] {10,2,2,2,3,4,5};
+            for(var i=0;i<numbers.Length;i++){
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
--- gradebook/src/GradeBook/obj/Debug/net8.0/GradeBook.AssemblyInfo.cs
+++ gradebook/src/GradeBook/obj/Debug/net8.0/GradeBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15aab47aea38f6a77aebe9d7890ca6ad37dd8f80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d280e18e482295ae270efbb83d3debf3ab492e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
